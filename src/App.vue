32877ddc6813ae41a364549b4309a846e2125dea
wu <whx1978938887@student.gdufe.edu.cn>
2023-02-02 23:30:13 +0800

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <router-view/>
-  <FooterVue/>
+  <router-view />
+  <FooterVue />
 </template>
 
 <script>
@@ -8,7 +8,7 @@ import FooterVue from './components/FooterVue.vue'
 
 export default {
   name: 'AppView',
-  mounted () {
+  mounted() {
     this.$router.push('/login')
   },
   components: {
